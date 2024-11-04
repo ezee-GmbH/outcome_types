5fe9ff6ca1172e55ed9bc70342bb811d46b6a43c
Max Luchterhand <max@luchterhand.io>
2024-11-04 12:35:44 +0100

    feature: bail functions

--- lib/src/result.dart
+++ lib/src/result.dart
@@ -6,11 +6,23 @@ import 'package:meta/meta.dart';
 /// an optional error value.
 ///
 /// Inspired by https://doc.rust-lang.org/std/result/
-sealed class Result<E, T> {}
+sealed class Result<E, T> {
+  const Result();
+
+  T operator ~() {
+    switch (this) {
+      case Success(value: final value):
+        return value;
+      case Failure(value: final failure):
+        // ignore: only_throw_errors
+        throw Failure<E, T>(failure);
+    }
+  }
+}
 
 /// Contains the success value.
 @immutable
-class Success<E, T> implements Result<E, T> {
+class Success<E, T> extends Result<E, T> {
   const Success(this.value);
 
   static Success<E, void> empty<E>() => Success<E, void>(null);
@@ -29,7 +41,7 @@ class Success<E, T> implements Result<E, T> {
 
 /// Optionally contains the error value.
 @immutable
-class Failure<E, T> implements Result<E, T> {
+class Failure<E, T> extends Result<E, T> {
   const Failure(this.value);
 
   static Failure<void, T> empty<T>() => Failure<void, T>(null);
@@ -43,5 +55,26 @@ class Failure<E, T> implements Result<E, T> {
   bool operator ==(Object other) {
     if (other case Failure<E, T>(:final value)) return this.value == value;
     return false;
+  }
+}
+
+/// An attempt to replicate Rusts question mark operator
+/// https://doc.rust-lang.org/std/result/#the-question-mark-operator-
+
+Result<E, T> bailOnFail<E, T>(Result<E, T> Function() f) {
+  try {
+    return f();
+  } on Failure<E, T> catch (failure) {
+    return failure;
+  }
+}
+
+Future<Result<E, T>> bailOnFailAsync<E, T>(
+  Future<Result<E, T>> Function() f,
+) async {
+  try {
+    return await f();
+  } on Failure<E, T> catch (failure) {
+    return failure;
   }
 }
